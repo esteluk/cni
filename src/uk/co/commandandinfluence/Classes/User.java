477d2e19317c980f9ab6cf7f8d5c7c267bb24b4f
Nathan Wong <nathan@nathanwong.co.uk>
2013-06-22 23:55:51 +0100

    Add missing stuff

---
+++ src/uk/co/commandandinfluence/Classes/User.java
@@ -0,0 +1,13 @@
+package uk.co.commandandinfluence.Classes;
+
+public class User {
+	public String id = "";
+	public String first_name = "";
+	public String last_name = "";
+	
+	
+	
+	public User() {
+		
+	}
+}
